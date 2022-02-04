f31c0ecd6322bcbef7a81db57df523c12dda5f74
luis-londono <43869947+luis-londono@users.noreply.github.com>
2022-02-04 15:36:38 -0600

    initial commit

---
+++ src/props/Child.tsx
@@ -0,0 +1,14 @@
+interface ChildProps {
+  color: string;
+}
+
+export const Child = ( { color }: ChildProps) => {
+  return (
+    <div>{color}</div>
+    );
+};
+
+export const ChildAsFC:React.FC<ChildProps> = ( { color } ) => {
+  return <div>{color}</div>
+};
+
