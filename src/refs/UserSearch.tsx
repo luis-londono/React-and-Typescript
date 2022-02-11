de894fe391091e62d04a7b5053324f5923e3c94c
luis-londono <43869947+luis-londono@users.noreply.github.com>
2022-02-11 15:06:06 -0600

    input ref

--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,5 +1,5 @@
 import { stringify } from 'querystring';
-import {useState} from 'react';
+import {useRef, useState, useEffect} from 'react';
 
 const users = [
   { name: 'Sarah', age: 20 },
@@ -7,12 +7,14 @@ const users = [
   { name: 'Michael', age: 20 },
 ]
 
+
+
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState('');
   const [user, setUser] = useState<{name: string, age: number} | undefined>();
 
-  
-const onClick = () => {
+  const onClick = () => {
   const foundUser = users.find((user) => {
     return user.name === name;
   })
@@ -20,10 +22,18 @@ const onClick = () => {
   setUser(foundUser);
 }
 
+  useEffect(() => {
+    if (!inputRef.current){
+      return;
+    }
+  
+    inputRef.current.focus();
+  }, []);
+
   return (
     <div>
       User Search
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
       <button onClick={onClick}>Find User</button>
       <div>
         {user && "First Name: " +  user.name} <br/>
